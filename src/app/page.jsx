e2a2bbad26afbd7e62f91f069d2f3efa41e5622c
Shrihari K N <shrihari.k.n17@gmail.com>
2025-05-05 21:19:28 +0530

    basic structure

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Hero from "../sections/Hero";
+export default function Home() {
+  return (
+    <>
+    {/* <h1>Welcome to EchoHub</h1>
+    <Header/> */}
+    <Hero/>
+    {/* <Features/>
+    <PreviewCommunities/>
+    <CTA/>
+    <Footer/> */}
+    </>
+  );
+}
+
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Hero(){
+    return <div className="heroSection">
+        <h1>Welcome to EchoHub</h1>
+        <p>Join your tribe, spark meaningful conversations, and explore mindboggling ideas!</p>
+    </div>
+
+
+}
+export default Hero 
