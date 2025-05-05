1d5b1b976085ef60fae3da851d2d7daf9c2e02ff
Shrihari K N <shrihari.k.n17@gmail.com>
2025-05-05 16:56:06 +0530

    initial changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
-        {children}
+       
       </body>
     </html>
   );
